806a0b7c89a8f7e8a2432bb0aabe08eba9263708
Jared Flater <jaredflater@jflater-air.student.iastate.edu>
2021-06-07 15:45:10 -0500

    ignore images

--- Code/ColumnGPSDataMapping.R
+++ Code/ColumnGPSDataMapping.R
@@ -4,10 +4,10 @@ library(exifr)
 library(tidyverse)
 library(ggmap)
 
-# SetWd to where images are
-setwd("Images/")
+
 # Image files, all column location pics are *number*C.jpeg
-files <- list.files(pattern = "*C.heic")
+setwd("../Images/")
+files <- list.files(pattern = "*.HEIC")
 
 # Read exif data, select filename, date, and GPS coordinates
 dat <- read_exif(files) %>%
@@ -15,18 +15,12 @@ dat <- read_exif(files) %>%
                SourceFile, DateTimeOriginal,
                GPSLongitude, GPSLatitude)
 
-write.csv(dat2, 'Exifdata.csv',
+write.csv(dat, 'Exifdata.csv',
           row.names = F)
 
-ggplot(dat, aes(x = GPSLongitude, y = GPSLatitude)) +
-  geom_point(aes(color = SourceFile)) +
+ggplot(dat, aes(x = GPSLongitude, y = GPSLatitude, label = SourceFile)) +
+  geom_point(aes()) +
+  geom_text(aes(label = SourceFile),hjust=0, vjust=0)
   ggtitle("Location of columns")
 
-us_counties <- map_data("county") %>%
-  filter(region == "iowa", subregion == "buchanan") 
-head(us_counties)
 
-p <- ggplot(data = us_counties,
-            mapping = aes(x = long, y = lat,
-                          group = group, fill = subregion))
-p
